383844d446748efb1c1d89330beceb62025cacad
Razvan Savescu <51855978+KukatanaMomori@users.noreply.github.com>
2023-10-28 22:51:03 +0300

    capy controls work

---
+++ rocky_remaster/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    private bool boost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            boost = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump, boost);
+        jump = false;
+        boost = false;
+        
+    }
+}
