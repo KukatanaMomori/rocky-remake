977f014172a098a61df641509ea373b20e480030
Razvan Savescu <51855978+KukatanaMomori@users.noreply.github.com>
2023-10-29 13:27:36 +0200

    added parallax, animations

--- rocky_remaster/Assets/scripts/PlayerMovement.cs
+++ rocky_remaster/Assets/scripts/PlayerMovement.cs
@@ -10,25 +10,34 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
     private bool boost;
-
+    private Material material;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();  
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (horizontalMove != 0f) {
+            animator.SetBool("isWalking", true);
+        }
+        else { animator.SetBool("isWalking", false);
 
-        if (Input.GetButtonDown("Jump"))
-        {
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            animator.SetBool("isJumping", true);
             jump = true;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+
+        else { animator.SetBool("isJumping", false); }
+        if (Input.GetKeyDown(KeyCode.Space)) {
             boost = true;
+            //animator.SetBool("isJumping", true);
         }
     }
     private void FixedUpdate()
